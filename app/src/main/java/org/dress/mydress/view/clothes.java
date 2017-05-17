9fba09d7bc17cc9d70b4b6afbaa4cbfdc24aa653
weihsiyi <taiwan27@@##12345>
2017-05-17 19:23:19 +0800

    [what] create project

---
+++ app/src/main/java/org/dress/mydress/view/clothes.java
@@ -0,0 +1,15 @@
+package org.dress.mydress.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.dress.mydress.R;
+
+public class clothes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothes);
+    }
+}
