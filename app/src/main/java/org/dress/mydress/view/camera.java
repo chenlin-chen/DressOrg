4d79406ddb800fcd9dddc06b91e9c5e66a82e221
chenlin-chen <motest1218@gmail.com>
2017-05-25 00:50:15 +0800

    [what]Start implement gallery
    [what]Start implement gallery
    [how] add function in camera

--- app/src/main/java/org/dress/mydress/view/camera.java
+++ app/src/main/java/org/dress/mydress/view/camera.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import org.dress.mydress.R;
@@ -15,18 +17,31 @@ public class camera extends AppCompatActivity {
     ImageView mImageView = null;
     Bitmap mBitmap = null;
     String mPhotoPath = null;
+    private int PICK_IMAGE_REQUEST = 1;
 
+    Button m_preedit_buttom ;
+    Button m_selectphoto_buttom ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        init();
+    }
+
+    private void init()
+    {
         mImageView = (ImageView) findViewById(R.id.imgDisplay);
+        m_preedit_buttom = (Button) findViewById(R.id.buttom_preedit);
+        m_selectphoto_buttom = (Button) findViewById(R.id.buttom_select);
+
+        CameraOnClickListener button_click_listener = new CameraOnClickListener();
+        m_preedit_buttom.setOnClickListener(button_click_listener);
+        m_selectphoto_buttom.setOnClickListener(button_click_listener);
         if(CheckPreActivityDeliverData())
         {
             mPhotoPath = GetPhotoPath();
             SetImageView(mPhotoPath);
         }
-
     }
 
     private boolean CheckPreActivityDeliverData()
@@ -58,4 +73,23 @@ public class camera extends AppCompatActivity {
         mImageView.setImageBitmap(mBitmap);
     }
 
+
+    class CameraOnClickListener implements View.OnClickListener{
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.buttom_preedit:
+                    break;
+                case R.id.buttom_select:
+                    StartSelectPhoto();
+                    break;
+            }
+        }
+    }
+
+    private void StartSelectPhoto()
+    {
+        //TODO: reference customer gallery(https://guides.codepath.com/android/Accessing-the-Camera-and-Stored-Media#file-pickers)
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
 }
